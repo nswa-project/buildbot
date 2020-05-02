cc5027ec3e871e02b002e1d41f337eb981f94613
nswa-project <41747840+nswa-project@users.noreply.github.com>
2020-05-02 21:30:50 +0800

    use prebuild speedup the build

---
+++ prebuild-dl.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+BASE_URL=https://github.com/nswa-project/buildbot/releases/download/prebuild-assets/prebuild-
+
+cd "$2"
+
+rm -rf build_dir staging_dir
+
+wget -O _.tar.gz "${BASE_URL}${1}.tar.gz" || exit 1
+tar xvf _.tar.gz || exit 1
+rm -f _.tar.gz
